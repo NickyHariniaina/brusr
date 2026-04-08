9a42879af0bcca678ac8d3e8148e5432de6680dc
NickyHariniai <ferrissushi@gmail.com>
2026-04-08 13:45:43 +0300

    chore: add parser for HTML

--- src/main.rs
+++ src/main.rs
@@ -30,4 +30,130 @@ fn elem(tag_name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
         node_type: NodeType::Element(ElementData { tag_name, attrs }),
     }
 }
+
+struct Parser {
+    pos: usize,
+    input: String,
+}
+
+impl Parser {
+    fn consume_char(&mut self) -> char {
+        let c = self.next_char();
+        self.pos += c.len_utf8();
+        c
+    }
+
+    fn consume_while(&mut self, test: impl Fn(char) -> bool) -> String {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        result
+    }
+
+    fn consume_whitespace(&mut self) {
+        self.consume_while(char::is_whitespace);
+    }
+
+    fn parse_name(&mut self) -> String {
+        self.consume_while(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9'))
+    }
+
+    fn next_char(&self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+
+    fn starts_with(&self, s: &str) -> bool {
+        self.input[self.pos..].starts_with(s)
+    }
+
+    fn expect(&mut self, s: &str) {
+        if self.starts_with(s) {
+            self.pos += s.len();
+        } else {
+            panic!("expected {}", s);
+        }
+    }
+
+    fn eof(&self) -> bool {
+        self.pos >= self.input.len()
+    }
+
+    fn parse_node(&mut self) -> Node {
+        if self.starts_with("<") {
+            self.parse_element()
+        } else {
+            self.parse_text()
+        }
+    }
+
+    fn parse_text(&mut self) -> Node {
+        text(self.consume_while(|c| c != '<'))
+    }
+
+    fn parse_element(&mut self) -> Node {
+        self.expect("<");
+        let tag_name = self.parse_name();
+        let attrs = self.parse_attributes();
+        self.expect(">");
+
+        let children = self.parse_nodes();
+
+        self.expect("</");
+        self.expect(&tag_name);
+        self.expect(">");
+
+        elem(tag_name, attrs, children)
+    }
+
+    fn parse_attr(&mut self) -> (String, String) {
+        let name = self.parse_name();
+        self.expect("=");
+        let value = self.parse_attr_value();
+        (name, value)
+    }
+
+    fn parse_attr_value(&mut self) -> String {
+        let open_quote = self.consume_char();
+        assert!(open_quote == '"' || open_quote == '\'');
+        let value = self.consume_while(|c| c != open_quote);
+        let close_quote = self.consume_char();
+        assert_eq!(close_quote, open_quote);
+        value
+    }
+
+    fn parse_attributes(&mut self) -> AttrMap {
+        let mut attributes = AttrMap::new();
+        loop {
+            self.consume_whitespace();
+            if self.next_char() == '>' {
+                break;
+            }
+            let (name, value) = self.parse_attr();
+            attributes.insert(name, value);
+        }
+        attributes
+    }
+
+    fn parse_nodes(&mut self) -> Vec<Node> {
+        let mut nodes = Vec::new();
+        loop {
+            self.consume_whitespace();
+            if self.eof() || self.starts_with("</") {
+                break;
+            }
+            nodes.push(self.parse_node());
+        }
+        nodes
+    }
+}
+
+fn parse(source: String) -> Node {
+    let mut nodes = Parser { pos: 0, input: source}.parse_nodes();
+    if nodes.len() == 1 {
+        return nodes.remove(0);
+    }
+    elem("html".to_string(), HashMap::new(), nodes)
+}
+
 fn main() {}
