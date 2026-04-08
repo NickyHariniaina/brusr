215fba5e782f01cbeade9378a5c1f39cfdeb6e98
NickyHariniai <ferrissushi@gmail.com>
2026-04-08 13:04:09 +0300

    chore: getting started

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+struct Node {
+    children: Vec<Node>,
+    node_type: NodeType,
+}
+
+enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+struct ElementData {
+    tag_name: String,
+    attrs: AttrMap,
+}
+
+type AttrMap = HashMap<String, String>;
+
+fn text(data: String) -> Node {
+    Node {
+        children: Vec::new(),
+        node_type: NodeType::Text(data),
+    }
+}
+
+fn elem(tag_name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children,
+        node_type: NodeType::Element(ElementData { tag_name, attrs }),
+    }
+}
+fn main() {}
